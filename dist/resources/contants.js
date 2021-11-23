d69a1f977cd2c573d839251fce369b92ca9cb88a
jimi <jamalmoosvi@gmail.com>
2021-11-23 21:55:02 +0500

    remove gitignore files

--- dist/resources/contants.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.dummyvariable = void 0;
-exports.dummyvariable = '12345';
-//# sourceMappingURL=contants.js.map
